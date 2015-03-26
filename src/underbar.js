55c6c0af77334cd050d830fc1ee1315669e8a39c
Paul Sokolik <paul.sokolik@gmail.com>
2015-03-26 16:53:57 -0500

    'completed reduce and all part I functions'

--- src/underbar.js
+++ src/underbar.js
@@ -172,6 +172,18 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    if(accumulator === undefined) {
+      accumulator = _.first(collection);
+      collection = _.last(collection, collection.length-1)
+      _.each(collection, function(item) {
+        accumulator = iterator(accumulator,item);
+      });
+    } else {
+      _.each(collection, function(item) {
+        accumulator = iterator(accumulator,item);
+      });
+    }
+    return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
